541a4406039e8f5effc2f9e89d5d6b960fbe5459
choi <younggeunn@naver.com>
2022-10-23 10:45:58 +0900

    flush 예제

--- src/main/java/choi/jpa/basic1/JpaMain.java
+++ src/main/java/choi/jpa/basic1/JpaMain.java
@@ -45,7 +45,10 @@ public class JpaMain {
         // writeBehind();
 
         // 변경감지
-        dirtyChecking();
+        //dirtyChecking();
+
+        // flush
+        flush();
     }
 
     static void saveMember() {
@@ -494,6 +497,37 @@ public class JpaMain {
             /**
              * DB를 조회해보면 변경된 것을 확인할 수 있다.
              */
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            // 자원반환
+            em.close();
+        }
+
+        // 자원반환
+        emf.close();
+    }
+
+    static void flush() {
+        // 선언
+        EntityManagerFactory emf
+                = Persistence.createEntityManagerFactory("hello"); // persistence.xml의 persistence-unit의 name
+        EntityManager em = emf.createEntityManager();
+
+        // 트랜잭션 선언 및 시작
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+            Member member = new Member();
+            member.setId(401L);
+            member.setName("사용자-401");
+            em.persist(member);
+
+            em.flush(); // flush()를 호출하는 시점에 Insert쿼리가 수행된다.
+
+            // 트랜잭션 커밋
+            tx.commit();
         } catch (Exception e) {
             tx.rollback();
         } finally {
