1fdc64ecc2e770a5135c6b966ca5d7216abc2ea4
choi <younggeunn@naver.com>
2022-10-23 18:46:33 +0900

    기본키 매핑 전략 예제

--- src/main/java/choi/jpa/basic1/Member.java
+++ src/main/java/choi/jpa/basic1/Member.java
@@ -7,6 +7,25 @@ import java.util.Date;
 public class Member {
 
     @Id // PK
+    /*
+        @GeneratedValue(strategy = GenerationType.AUTO)
+        - GenerationType.TABLE, GenerationType.SEQUENCE, GenerationType.IDENTITY 중 데이터베이스 방언에 따라 한 개를 선택하는 전략이다.
+     */
+    /*
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        - 기본키 생성을 데이터베이스에 위임하는 전략이다.
+        - JPA는 보통 트랜잭션 커밋 시점에 INSERT를 수행하는데 IDENTITY전략을 사용하면 em.persist() 시점에 INSERT를 수행한다.
+          : 영속성 컨텍스트에 값을 저장할 때, id값이 필요한데 기본키 생성을 데이터베이스에 위임하기 때문에 INSERT이후에 id를 알 수 있기 때문.
+            이로인해 em.persist() 시점에 INSERT를 수행한다. (때문에 버퍼를 쌓은 후 일괄적으로 commit 불가)
+     */
+    /*
+        @GeneratedValue(strategy = GenerationType.SEQUENCE)
+        - 시퀀스 오브젝트를 통해 값을 생성하고 셋팅하는 전략이다.
+     */
+    /*
+        @GeneratedValue(strategy = GenerationType.TABLE)
+        - 키 생성 전용 테이블을 만들어서 데이터베이스 시퀀스를 흉내내는 전략이다.
+     */
     private Long id;
 
     @Column(name = "name") // DB 컬럼명을 명시할 수 있다.(DB컬럼명과 변수명이 다를 경우 이와 같이 사용할 수 있다.)
