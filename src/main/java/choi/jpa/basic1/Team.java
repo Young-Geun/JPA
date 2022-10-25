110c48100e022ad315c77884044521b2855a0909
choi <younggeunn@naver.com>
2022-10-25 20:52:26 +0900

    객체 지향 모델링 예제 - 양방향 연관관계 - mappedBy 설명 추가

--- src/main/java/choi/jpa/basic1/Team.java
+++ src/main/java/choi/jpa/basic1/Team.java
@@ -14,6 +14,13 @@ public class Team {
 
     private String name;
 
+    /*
+        - mappedBy
+        : 양방향 연관관계에서는 객체의 두 관계를 제어하는 연관관계의 주인을 결정해야한다. (Player or Team)
+          연관관계의 주인만이 외래 키를 관리(등록, 수정)할 수 있다.
+          주인이 아닌쪽에 mappedBy 속성을 이용하여 주인을 정한다.
+          아래 mappedBy = "team"에서 team은 Player.java의 team 변수를 뜻한다.
+     */
     @OneToMany(mappedBy = "team")
     private List<Player> players = new ArrayList<>();
 
