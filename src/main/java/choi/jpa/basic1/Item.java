a8adec66c436f050d53c4f84c1f3079a3dd8f5b5
choi <younggeunn@naver.com>
2022-10-27 21:50:38 +0900

    상속관계 매핑 예제 - 단일테이블 전략

--- src/main/java/choi/jpa/basic1/Item.java
+++ src/main/java/choi/jpa/basic1/Item.java
@@ -3,8 +3,9 @@ package choi.jpa.basic1;
 import javax.persistence.*;
 
 @Entity
-@Inheritance(strategy = InheritanceType.JOINED) // 조인 전략
-@DiscriminatorColumn
+// @Inheritance(strategy = InheritanceType.JOINED) // 조인 전략
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE) // 단일테이블 전략 (해당 전략에서는 @DiscriminatorColumn을 생략해도 DTYPE이 자동 생성된다.)
+@DiscriminatorColumn // DTYPE컬럼 생성 옵션
 public class Item {
 
     @Id
--- src/main/java/choi/jpa/basic1/JpaMain4.java
+++ src/main/java/choi/jpa/basic1/JpaMain4.java
@@ -10,8 +10,11 @@ public class JpaMain4 {
     public static void main(String[] args) {
         /** 상속관계 매핑 예제 */
 
-        // 조인전략
-        ex1();
+        // 조인 전략
+        // ex1();
+
+        // 단일테이블 전략
+        ex2();
     }
 
     static void ex1() {
@@ -65,6 +68,62 @@ public class JpaMain4 {
                             on movie0_.id=movie0_1_.id
                     where
                         movie0_.id=?
+             */
+
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+
+        emf.close();
+    }
+
+    static void ex2() {
+        // 선언
+        EntityManagerFactory emf
+                = Persistence.createEntityManagerFactory("hello");
+        EntityManager em = emf.createEntityManager();
+
+        // 트랜잭션 선언 및 시작
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+            Movie movie = new Movie();
+            movie.setDirector("감독 A");
+            movie.setActor("배우 A");
+            movie.setName("스프링부트");
+            movie.setPrice(10_000);
+
+            em.persist(movie);
+            /*
+                테이블 조회 시, 아래와 같이 데이터가 생성되어있음
+                - ITEM 테이블 (모든 상속관계가 1개의 테이블로 관리되어서 아래와 같은 형태를 가진다.)
+                    DTYPE  	ID  	NAME  	PRICE  	ARTIST  	AUTHOR  	ISBN  	ACTOR  	DIRECTOR
+                    Movie	1	    스프링부트	10000	null	    null	    null	배우 A	감독 A
+             */
+
+            // 1차 캐시 삭제
+            em.flush();
+            em.clear();
+
+            // 조회 시, 콘솔창의 쿼리를 확인해보면 한 개의 테이블에서 가져오는 것을 확인할 수 있다. (성능상 장점이 있음)
+            Movie findMovie = em.find(Movie.class, movie.getId());
+            /*
+                Hibernate:
+                    select
+                        movie0_.id as id2_0_0_,
+                        movie0_.name as name3_0_0_,
+                        movie0_.price as price4_0_0_,
+                        movie0_.actor as actor8_0_0_,
+                        movie0_.director as director9_0_0_
+                    from
+                        Item movie0_
+                    where
+                        movie0_.id=?
+                        and movie0_.DTYPE='Movie'
              */
 
             tx.commit();
