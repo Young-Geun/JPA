6d8c41ef9cc2ff86c7c6ff474a43d47dffade4fd
choi <younggeunn@naver.com>
2022-10-29 21:16:29 +0900

    JPA 기본 : 실전예제6 - 값 타입 매핑

--- src/main/java/choi/jpa/domain/Delivery.java
+++ src/main/java/choi/jpa/domain/Delivery.java
@@ -9,15 +9,43 @@ public class Delivery extends BaseEntity {
     @GeneratedValue
     private Long id;
 
-    private String city;
-
-    private String street;
-
-    private String zipcode;
-
     private DeliveryStatus status;
+
+    private Address address;
 
     @OneToOne(mappedBy = "delivery", fetch = FetchType.LAZY)
     private Order order;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public DeliveryStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(DeliveryStatus status) {
+        this.status = status;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
 
 }
