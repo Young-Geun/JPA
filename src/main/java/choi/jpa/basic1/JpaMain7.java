b8e463452caad1df73edbcc6099697b0eb80dd31
choi <younggeunn@naver.com>
2022-10-30 12:14:17 +0900

    객체지향 쿼리 언어(기본 문법) 예제 - Criteria 기본 예제

--- src/main/java/choi/jpa/basic1/JpaMain7.java
+++ src/main/java/choi/jpa/basic1/JpaMain7.java
@@ -4,6 +4,9 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
 import java.util.List;
 
 public class JpaMain7 {
@@ -12,7 +15,10 @@ public class JpaMain7 {
         /** 객체지향 쿼리 언어 - 기본 문법 */
 
         // JPQL 기본
-        ex1();
+        // ex1();
+
+        // Criteria 기본
+        ex2();
     }
 
     static void ex1() {
@@ -39,7 +45,7 @@ public class JpaMain7 {
             tx.commit();
 
             /*
-                실행 쿼리
+                - 실행 쿼리
                  select
                     player0_.id as id1_11_,
                     player0_.createdBy as createdB2_11_,
@@ -51,6 +57,65 @@ public class JpaMain7 {
                     Player player0_
                 where
                     player0_.USERNAME like '%c%'
+             */
+        } catch (Exception e) {
+            tx.rollback();
+            e.printStackTrace();
+        } finally {
+            em.close();
+        }
+
+        emf.close();
+    }
+
+    static void ex2() {
+        // 선언
+        EntityManagerFactory emf
+                = Persistence.createEntityManagerFactory("hello");
+        EntityManager em = emf.createEntityManager();
+
+        // 트랜잭션 선언 및 시작
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+            /*
+                - Criteria
+                  JPQL은 결국 문자열로 된 쿼리를 만들기 때문에 오류 가능성이 높다.
+                  이에 반해 Criteria를 사용하면 컴파일 시점에서 오류를 잡을 수 있다는 장점이 있다.
+                  또한 동적 쿼리를 보다 안전하게 작성할 수 있다.
+                  단, 직관적이지 않아서 실용성이 없다.
+             */
+            // Criteria 사용 준비
+            CriteriaBuilder cb = em.getCriteriaBuilder();
+            CriteriaQuery<Player> query = cb.createQuery(Player.class);
+
+            // 루트 클래스 (조회를 시작할 클래스)
+            Root<Player> p = query.from(Player.class);
+
+            // 쿼리 생성
+            CriteriaQuery<Player> cq = query.select(p).where(cb.equal(p.get("name"), "choi"));
+            List<Player> players = em.createQuery(cq).getResultList();
+
+            for (Player player : players) {
+                System.out.println("player = " + player.getName());
+            }
+
+            tx.commit();
+
+            /*
+                - 실행 쿼리
+                 select
+                    player0_.id as id1_11_,
+                    player0_.createdBy as createdB2_11_,
+                    player0_.modifiedBy as modified3_11_,
+                    player0_.LOCKER_ID as LOCKER_I5_11_,
+                    player0_.USERNAME as USERNAME4_11_,
+                    player0_.TEAM_ID as TEAM_ID6_11_
+                 from
+                    Player player0_
+                 where
+                    player0_.USERNAME=?
              */
         } catch (Exception e) {
             tx.rollback();
