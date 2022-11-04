9f75cd9f5d9338ac59eb50160fc867c9ffd9a94c
choi <younggeunn@naver.com>
2022-11-04 21:52:08 +0900

    JPA 활용1 - 사용자 저장, 조회 기능 추가 및 테스트 코드 추가

--- src/main/java/choi/jpa/repository/MemberRepository.java
+++ src/main/java/choi/jpa/repository/MemberRepository.java
@@ -13,6 +13,15 @@ public class MemberRepository {
 
     private final EntityManager em;
 
+    public Long save(Member member) {
+        em.persist(member);
+        return member.getId();
+    }
+
+    public Member find(Long id) {
+        return em.find(Member.class, id);
+    }
+
     public List<Member> findAll() {
         return em.createQuery("select m from Member m", Member.class)
                 .getResultList();
--- src/test/java/choi/jpa/controller/MemberControllerTest.java
+++ src/test/java/choi/jpa/controller/MemberControllerTest.java
@@ -1,24 +1,30 @@
 package choi.jpa.controller;
 
 import choi.jpa.domain.Member;
+import choi.jpa.repository.MemberRepository;
 import choi.jpa.service.MemberService;
+import org.assertj.core.api.Assertions;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 import javax.persistence.EntityManager;
 import java.util.List;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-@Transactional
+@Transactional // test에 있으면 데이터를 롤백시킴
 public class MemberControllerTest {
 
     @Autowired
     MemberService memberService;
+
+    @Autowired
+    MemberRepository memberRepository;
 
     @Autowired
     EntityManager em;
@@ -28,4 +34,21 @@ public class MemberControllerTest {
         List<Member> lists = memberService.findMembers();
         assertEquals(0, lists.size());
     }
+
+    @Test
+    @Rollback(value = false) // DB의 데이터를 직접 보고 싶을 때, 설정
+    public void save() {
+        // given
+        Member member = new Member();
+        member.setName("choi");
+
+        // when
+        Long savedId = memberRepository.save(member);
+        Member findMember = memberRepository.find(savedId);
+
+        // then
+        Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
+        Assertions.assertThat(findMember.getName()).isEqualTo(member.getName());
+    }
+
 }
