55ace5e01e64f947e9919adc86cf923faf54e3a9
choi <younggeunn@naver.com>
2022-11-06 14:31:51 +0900

    JPA 활용1 - 엔티티 설계 시 주의사항 적용

--- src/main/java/choi/jpa/domain/Order.java
+++ src/main/java/choi/jpa/domain/Order.java
@@ -19,14 +19,14 @@ public class Order {
     @Column(name = "order_id")
     private Long id;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "member_id")
     private Member member;
 
-    @OneToMany(mappedBy = "order")
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL) // cascade = CascadeType.ALL : Order만 저장(수정, 삭제)해도 연관된 OrderItem도 자동 저장되는 옵션
     private List<OrderItem> orderItems = new ArrayList<>();
 
-    @OneToOne
+    @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     @JoinColumn(name = "delivery_id")
     private Delivery delivery;
 
@@ -34,5 +34,22 @@ public class Order {
 
     @Enumerated(EnumType.STRING)
     private OrderStatus status; // 주문상태 [ORDER, CANCEL]
+
+    /** 연관관계 메서드 Start */
+    public void setMember(Member member) {
+        this.member = member;
+        member.getOrders().add(this);
+    }
+
+    public void addOrderItem(OrderItem orderItem) {
+        orderItems.add(orderItem);
+        orderItem.setOrder(this);
+    }
+
+    public void setDelivery(Delivery delivery) {
+        this.delivery = delivery;
+        delivery.setOrder(this);
+    }
+    /** 연관관계 메서드 End */
 
 }
