7c3967a7e80b90662f1b80bc8e978310d0239d7f
choi <younggeunn@naver.com>
2022-11-06 21:22:46 +0900

    JPA 활용1 - 상품 도메인

--- src/main/java/choi/jpa/domain/item/Item.java
+++ src/main/java/choi/jpa/domain/item/Item.java
@@ -1,6 +1,7 @@
 package choi.jpa.domain.item;
 
 import choi.jpa.domain.Category;
+import choi.jpa.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -28,5 +29,20 @@ public abstract class Item {
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
+
+    /** 비즈니스 로직 */
+    // stock 증가
+    public void addStock(int quantity) {
+        this.stockQuantity += quantity;
+    }
+
+    // stock 감소
+    public void removeStock(int quantity) {
+        int restStock = this.stockQuantity - quantity;
+        if (restStock < 0) {
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity = restStock;
+    }
 
 }
