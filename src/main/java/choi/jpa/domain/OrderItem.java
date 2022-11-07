99ed42c9fd7c4ff663aece1e7a01b606034f4323
choi <younggeunn@naver.com>
2022-11-07 21:16:22 +0900

    JPA 활용1 - 주문, 주문상품 엔티티 수정

--- src/main/java/choi/jpa/domain/OrderItem.java
+++ src/main/java/choi/jpa/domain/OrderItem.java
@@ -28,4 +28,30 @@ public class OrderItem {
 
     private int count; // 주문 수량
 
+    /** 생성 메서드 Start */
+    public static OrderItem createOrderItem(Item item, int orderPrice, int count) {
+        OrderItem orderItem = new OrderItem();
+        orderItem.setItem(item);
+        orderItem.setOrderPrice(orderPrice);
+        orderItem.setCount(count);
+
+        item.removeStock(count);
+
+        return orderItem;
+    }
+    /** 생성 메서드 End */
+
+    /** 비즈니스 로직 Start */
+    public void cancel() {
+        getItem().addStock(count);
+    }
+    /** 비즈니스 로직 End */
+
+    /** 조회 로직 Start */
+    // 전체 주문 가격 조회
+    public int getTotalPrice() {
+        return getOrderPrice() * getCount();
+    }
+    /** 조회 로직 End */
+
 }
