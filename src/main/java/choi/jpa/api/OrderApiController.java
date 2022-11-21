d7ca721377c8bbdb83f2316685d25e560324b965
choi <younggeunn@naver.com>
2022-11-21 21:54:41 +0900

    JPA 활용2 - 주문 조회 V6 : JPA에서 DTO 직접 조회(플랫 데이터 최적화)

--- src/main/java/choi/jpa/api/OrderApiController.java
+++ src/main/java/choi/jpa/api/OrderApiController.java
@@ -6,6 +6,8 @@ import choi.jpa.domain.OrderItem;
 import choi.jpa.domain.OrderStatus;
 import choi.jpa.repository.OrderRepository;
 import choi.jpa.repository.OrderSearch;
+import choi.jpa.repository.order.query.OrderFlatDto;
+import choi.jpa.repository.order.query.OrderItemQueryDto;
 import choi.jpa.repository.order.query.OrderQueryDto;
 import choi.jpa.repository.order.query.OrderQueryRepository;
 import lombok.Data;
@@ -17,7 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.time.LocalDateTime;
 import java.util.List;
 
-import static java.util.stream.Collectors.toList;
+import static java.util.stream.Collectors.*;
 
 /**
  *
@@ -112,11 +114,27 @@ public class OrderApiController {
 
     /**
      * V5. JPA에서 DTO로 바로 조회, 컬렉션 1 조회 최적화 버전 (1 + 1 Query)
-     *     - 페이징 가능
+     * - 페이징 가능
      */
     @GetMapping("/api/v5/orders")
     public List<OrderQueryDto> ordersV5() {
         return orderQueryRepository.findAllByDto_optimization();
+    }
+
+    /**
+     * V6. JPA에서 DTO로 바로 조회, 플랫 데이터(1Query) (1 Query)
+     * - 페이징 불가능
+     */
+    @GetMapping("/api/v6/orders")
+    public List<OrderQueryDto> ordersV6() {
+        List<OrderFlatDto> flats = orderQueryRepository.findAllByDto_flat();
+
+        return flats.stream()
+                .collect(groupingBy(o -> new OrderQueryDto(o.getOrderId(), o.getName(), o.getOrderDate(), o.getOrderStatus(), o.getAddress()),
+                        mapping(o -> new OrderItemQueryDto(o.getOrderId(), o.getItemName(), o.getOrderPrice(), o.getCount()), toList())
+                )).entrySet().stream()
+                .map(e -> new OrderQueryDto(e.getKey().getOrderId(), e.getKey().getName(), e.getKey().getOrderDate(), e.getKey().getOrderStatus(), e.getKey().getAddress(), e.getValue()))
+                .collect(toList());
     }
 
     @Data
