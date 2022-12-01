9bd172268aafd0068e8961c919d0916d5d7fff58
choi <younggeunn@naver.com>
2022-12-01 20:34:08 +0900

    - 실전! 스프링 데이터 JPA : 순수 JPA 기반 리포지토리 만들기

---
+++ src/main/java/choi/jpa/repository/TeamRepository.java
@@ -0,0 +1,40 @@
+package choi.jpa.repository;
+
+import choi.jpa.entity.Team;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public class TeamRepository {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    public Team save(Team team) {
+        em.persist(team);
+        return team;
+    }
+
+    public void delete(Team team) {
+        em.remove(team);
+    }
+
+    public long count() {
+        return em.createQuery("select count(t) from Team t", Long.class).getSingleResult();
+    }
+
+    public List<Team> findAll() {
+        // JPQL
+        return em.createQuery("select t from Team t", Team.class).getResultList();
+    }
+
+    public Optional<Team> findById(Long id) {
+        Team Team = em.find(Team.class, id);
+        return Optional.ofNullable(Team);
+    }
+
+}
