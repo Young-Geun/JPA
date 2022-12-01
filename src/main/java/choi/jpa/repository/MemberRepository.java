90a9930064d4edc3a36e913cd42734e91b1b46ce
choi <younggeunn@naver.com>
2022-12-01 21:05:12 +0900

    - 실전! 스프링 데이터 JPA : 공통 인터페이스 분석

--- src/main/java/choi/jpa/repository/MemberRepository.java
+++ src/main/java/choi/jpa/repository/MemberRepository.java
@@ -3,6 +3,10 @@ package choi.jpa.repository;
 import choi.jpa.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    List<Member> findByUsername(String username);
 
 }
