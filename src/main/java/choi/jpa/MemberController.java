219c2421257e2463f9bd088f7ef438c33647a17f
choi <younggeunn@naver.com>
2022-12-07 23:23:29 +0900

    - 실전! 스프링 데이터 JPA : Web 확장 - 도메인 클래스 컨버터

---
+++ src/main/java/choi/jpa/MemberController.java
@@ -0,0 +1,36 @@
+package choi.jpa;
+
+import choi.jpa.entity.Member;
+import choi.jpa.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.PostConstruct;
+
+@RestController
+@RequiredArgsConstructor
+public class MemberController {
+
+    private final MemberRepository memberRepository;
+
+    @PostConstruct
+    public void init() {
+        memberRepository.save(new Member("userA"));
+    }
+
+    // 도메인 클래스 컨버터 사용 전
+    @GetMapping("/members/{id}")
+    public String findMember(@PathVariable("id") Long id) {
+        Member member = memberRepository.findById(id).get();
+        return member.getUsername();
+    }
+
+    // 도메인 클래스 컨버터 사용 후
+    @GetMapping("/members2/{id}")
+    public String findMember2(@PathVariable("id") Member member) {
+        return member.getUsername();
+    }
+
+}
