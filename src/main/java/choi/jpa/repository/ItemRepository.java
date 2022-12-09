9683c718b4d1f06d3b7dae3aebc85d653a4be325
choi <younggeunn@naver.com>
2022-12-09 21:29:48 +0900

    - 실전! 스프링 데이터 JPA : 새로운 엔티티를 구별하는 방법

---
+++ src/main/java/choi/jpa/repository/ItemRepository.java
@@ -0,0 +1,8 @@
+package choi.jpa.repository;
+
+import choi.jpa.entity.Item;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ItemRepository extends JpaRepository<Item, Long> {
+
+}
