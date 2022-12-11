f0f9434d6b2e0f792eefdacbaf66ed4fa23d3a8b
choi <younggeunn@naver.com>
2022-12-11 13:38:29 +0900

    - 실전! 스프링 데이터 JPA : Projections(인터페이스 기반 Open Proejctions)

--- src/main/java/choi/jpa/repository/UsernameOnly.java
+++ src/main/java/choi/jpa/repository/UsernameOnly.java
@@ -1,7 +1,16 @@
 package choi.jpa.repository;
 
+import org.springframework.beans.factory.annotation.Value;
+
 public interface UsernameOnly {
 
+    @Value("#{target.username + ' ' + target.age + ' ' + target.team.name}")
     String getUsername();
+
+    /*
+        Open Proejctions
+        => @Value 사용
+           : Closed Projections 과 다르게 모든 필드를 가져온 후 명시된 필드를 추출
+     */
 
 }
