d7f88a49c4a196cb04f3b5f5c032917edfb56fc1
choi <younggeunn@naver.com>
2022-12-16 07:01:11 +0900

    실전! Querydsl - 조인 (기본 조인)

--- src/test/java/choi/jpa/querydsl/QuerydslBasicTest.java
+++ src/test/java/choi/jpa/querydsl/QuerydslBasicTest.java
@@ -2,6 +2,7 @@ package choi.jpa.querydsl;
 
 import choi.jpa.querydsl.entity.Member;
 import choi.jpa.querydsl.entity.QMember;
+import choi.jpa.querydsl.entity.QTeam;
 import choi.jpa.querydsl.entity.Team;
 import com.querydsl.core.QueryResults;
 import com.querydsl.core.Tuple;
@@ -272,6 +273,45 @@ public class QuerydslBasicTest {
 
         Assertions.assertThat(teamB.get(team.name)).isEqualTo("teamB");
         Assertions.assertThat(teamB.get(member.age.avg())).isEqualTo(35);
+    }
+
+    /**
+     * 팀 A에 소속된 모든 회원
+     */
+    @Test
+    public void join() throws Exception {
+        QMember member = QMember.member;
+        QTeam team = QTeam.team;
+
+        List<Member> result = queryFactory
+                .selectFrom(member)
+                .join(member.team, team)
+                .where(team.name.eq("teamA"))
+                .fetch();
+
+        Assertions.assertThat(result)
+                .extracting("username")
+                .containsExactly("member1", "member2");
+    }
+
+    /**
+     * 세타 조인(연관관계가 없는 필드로 조인)
+     * 회원의 이름이 팀 이름과 같은 회원 조회
+     */
+    @Test
+    public void theta_join() throws Exception {
+        em.persist(new Member("teamA"));
+        em.persist(new Member("teamB"));
+
+        List<Member> result = queryFactory
+                .select(member)
+                .from(member, team)
+                .where(member.username.eq(team.name))
+                .fetch();
+
+        Assertions.assertThat(result)
+                .extracting("username")
+                .containsExactly("teamA", "teamB");
     }
 
 }
